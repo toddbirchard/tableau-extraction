0d8bc6c7a5c4dfd372cf61c72d1306161ec0bc56
Todd Birchard <toddbirchard@gmail.com>
2018-12-30 05:11:52 +0100

    Update main.py

--- application/main.py
+++ application/main.py
@@ -1,58 +1,65 @@
 import requests
-import xml
-import xmltodict
 import json
-from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
 
 
-class ExtractView:
+class ExtractTableauView:
     """Class instance for extracting a single Tableau View."""
 
-    token = ''
-    siteID = ''
-    namespaces = {
-        '@token': 'token',
-        '@id': 'id'
-     }
-
     def __init__(self, base_url, username, password, tableau_site):
-        """Initialize instance."""
+        """Initialize Taleau site instance for pillaging sequence."""
         self.url = base_url
         self.user = username
         self.password = password
         self.sitename = tableau_site
+        self.xml = self.initialize_tableau_request()
+        self.token = self.get_token()
+        self.site = self.get_site()
+        self.views = self.list_views()
 
     def get_view(self, view):
-        """Get all data contained in a single view."""
-        # Authorize using our generated token
-        headers = {'X-Tableau-Auth': token}
-        r = requests.get(self.url + '/api/3.0/sites/' + self.siteID +'/views/' + view + '/data', auth=(self.user, self.password), headers=headers)
+        """Get contents of a single view."""
+        headers = {'X-Tableau-Auth': self.token}
+        r = requests.get(self.url + '/api/3.0/sites/' + self.site +'/views/' + view + '/data', auth=(self.user, self.password), headers=headers)
+        print(r.content)
         return r.content
 
-    def get_all_views_in_site(self):
-        """List every view contained in a Tableau Site."""
-        # Authorize using our generated token
-        headers = {'X-Tableau-Auth': token}
-        # Execute API request
-        r = requests.get(self.url + '/api/3.0/auth/signin', auth=(self.user, self.password), headers=headers)
-        result = r.content
-        print(result)
+    def list_views(self):
+        """Get all views belonging to a Tableau Site."""
+        headers = {'X-Tableau-Auth': self.token}
+        r = requests.get(self.url + '/api/3.0/sites/' + self.site +'/views', auth=(self.user, self.password), headers=headers)
+        root = ET.fromstring(r.content)
+        views_arr = []
+        for child in root.iter('*'):
+            if child.tag == '{http://tableau.com/api}views':
+                for view in child:
+                    views_arr.append(view.attrib.get('id'))
+        return views_arr
 
-    def auth_token(self):
+    def get_token(self):
         """Receive Auth token to perform API requests."""
-        # Oh great, XML. My favorite.
+        for child in self.xml.iter('*'):
+            if child.tag == '{http://tableau.com/api}credentials':
+                token = child.attrib.get('token')
+                return token
+
+    def get_site(self):
+        """Retrieve ID of Tableau site instance."""
+        for child in self.xml.iter('*'):
+            if child.tag == '{http://tableau.com/api}site':
+                site = child.attrib.get('id')
+                return site
+
+    def initialize_tableau_request(self):
+        """Retrieve core XML for interacting with Tableau."""
         headers = {'Content-Type': 'application/xml'}
         # Pass your username, password, and Tableau "site" name
         body = '<tsRequest><credentials name="'+ self.user + '" password="'+ self.password + '" ><site contentUrl="' + self.sitename + '" /></credentials></tsRequest>'
         # Execute API request
         r = requests.post(self.url + '/api/3.0/auth/signin', auth=(self.user, self.password), headers=headers, data=body)
-        xmltree = ElementTree.fromstring(r.content)
-        result = json.dumps(xmltodict.parse(r.content), indent=4)
-        clean_dict = {key.strip(): item.strip() for key, item in result.items()}
-        self.token = clean_dict['tsResponse']['credentials']['@token']
-        self.siteID = clean_dict['tsResponse']['credentials']['site']['@id']
-        return result
+        root = ET.fromstring(r.content)
+        return root
 
 
-view = ExtractView('http://charthub.io', 'todd', 'flyeaglesfly', 'Hackers')
-view.auth_token()
+tableau_view_extractor = ExtractView('[MyTaleauServerURL]', '[username]', '[password]', '[sitename]')
+tableau_view_extractor.get_view('[ViewIDOfYourChoice')
