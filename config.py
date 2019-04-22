941c3d3db0a892765a7a2988b139548a7cd5d88e
Todd Birchard <toddbirchard@gmail.com>
2019-04-22 05:53:02 -0400

    updated packages, refctored config

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 import os
-from configparser import ConfigParser
-
 
 class Config:
     """Parse config values from .ini."""
-
-    configParser = ConfigParser()
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    configFilePath = (os.path.join(dir_path, 'config.ini'))
-    configParser.read(configFilePath)
 
     # Testing vars
     DEBUG = True
@@ -17,10 +10,10 @@ class Config:
     FLASK_ASSETS_USE_CDN = True
     # Database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = configParser.get("DATABASE", "SQLALCHEMY_DATABASE_URI")
-    REDIS_URL = configParser.get("DATABASE", "REDIS_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    REDIS_URL = os.environ.get("REDIS_URI")
     # CREDENTIALS
-    BASE_URL = configParser.get("TABLEAU", "BASE_URL")
-    USERNAME = configParser.get("TABLEAU", "USERNAME")
-    PASSWORD = configParser.get("TABLEAU", "PASSWORD")
-    CONTENT_URL = configParser.get("TABLEAU", "CONTENT_URL")
+    BASE_URL = os.environ.get("BASE_URL")
+    USERNAME = os.environ.get("USERNAME")
+    PASSWORD = os.environ.get("PASSWORD")
+    CONTENT_URL = os.environ.get("CONTENT_URL")
